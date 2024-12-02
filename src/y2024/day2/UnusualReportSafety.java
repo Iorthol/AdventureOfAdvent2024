1d5adbc9262de56794ff6f132c31a19913a4ddf1
Bryce <hippy263@msn.com>
2024-12-02 12:52:10 -0500

    Yep

--- src/y2024/day2/UnusualReportSafety.java
+++ src/y2024/day2/UnusualReportSafety.java
@@ -1,4 +1,12 @@
 package y2024.day2;
 
+import y2024.AdeventFileReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnusualReportSafety {
+    List<String> unusualReportFile = AdeventFileReader.readFile("input-advent2024-day2.txt");
+    List<List<Integer>> unusualReport = new ArrayList<>();
+
 }
